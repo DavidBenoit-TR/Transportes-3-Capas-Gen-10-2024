8dffa3a7e969ab2afc561119fd8198204b614b1d
David Benoit <mdavid.benoit@gmail.com>
2024-09-17 13:00:49 -0600

    [Add] - BLL:BLL_Camiones Se agrega método para consultar y devolver listado de camiones + Se crea biblioteca de Clases

---
+++ BLL/BLL_Camiones.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace BLL
+{
+    public class BLL_Camiones
+    {
+        //CREATE
+        //READ
+        public static List<Camiones_VO> get_Camiones(params object[] parametros)
+        {
+            return DAL_Camiones.get_Camiones(parametros);
+        }
+        //UPDATE
+        //DELETE
+    }
+}
