164c2fc914eb93c68f84c4811d95075b16bfa58c
David Benoit <mdavid.benoit@gmail.com>
2024-09-18 09:16:59 -0600

    [Add] - BLL: CRUD completo de Camiones

--- BLL/BLL_Camiones.cs
+++ BLL/BLL_Camiones.cs
@@ -11,12 +11,24 @@ namespace BLL
     public class BLL_Camiones
     {
         //CREATE
+        public static string insertar_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.insertar_Camion(camion);
+        }
         //READ
         public static List<Camiones_VO> get_Camiones(params object[] parametros)
         {
             return DAL_Camiones.get_Camiones(parametros);
         }
         //UPDATE
+        public static string update_Camion(Camiones_VO camion)
+        {
+            return DAL_Camiones.update_Camion(camion);
+        }
         //DELETE
+        public static string delete_Camion(int id)
+        {
+            return DAL_Camiones.delete_Camion(id);
+        }
     }
 }
